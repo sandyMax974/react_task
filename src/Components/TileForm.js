d469fb6a50c1dee9ebb73e2aedc6f175b2448e79
sandy <pounoussamy.sandy@gmail.com>
2021-06-25 12:50:44 +0100

    Fixed error with event handler value and react-state

--- src/Components/TileForm.js
+++ src/Components/TileForm.js
@@ -2,46 +2,110 @@ import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 import TilesDataService from "../Services/TilesData.service";
 
-const TileForm = () => {
-  const [launchDate, setLaunchDate] = useState("");
-  const [status, setStatus] = useState("");
 
-  const onSubmit = async (e) => {
+class TileForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: '',
+      status: ''
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  async handleSubmit(e) {
+    const { date, status } = this.state;
     e.preventDefault();
-    console.log(e.target.value);
-    await TilesDataService.createTile(launchDate, status);
-    // post method to API with form data
-  };
+    console.log(date, status)
+    await TilesDataService.createTile(date, status)
+  }
 
-  return (
-    <Form onSubmit={onSubmit}>
-      <Form.Group>
-        <Form.Label>Launch Date</Form.Label>
-        <Form.Control
-          id="date-input"
-          type="date"
-          onChange={(e) => setLaunchDate(e.target.value)}
+  handleInputChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+    console.log("Inputs", this)
+  }
+
+  render() {
+    return (
+      <Form onSubmit={this.handleSubmit}>
+        <Form.Group>
+         <Form.Label>Launch Date</Form.Label>
+         <Form.Control
+           name="date"
+           type="date"
+          onChange={this.handleInputChange}
         />
-      </Form.Group>
+       </Form.Group>
 
-      <Form.Group>
-        <Form.Label>Status</Form.Label>
-        <Form.Control
-          id="status-select"
+       <Form.Group>
+         <Form.Label>Status</Form.Label>
+         <Form.Control
+          name="status"
           as="select"
-          onChange={(e) => setStatus(e.target[1].value)}
+          onChange={this.handleInputChange}
         >
           <option>-- Choose an option --</option>
-          <option value="PENDING">Pending</option>
-          <option value="ARCHIVE">Archive</option>
-          <option value="LIVE">Live</option>
+          <option value="pending">Pending</option>
+          <option value="archive">Archive</option>
+          <option value="live">Live</option>
         </Form.Control>
       </Form.Group>
       <Button variant="primary" type="submit">
         Submit
       </Button>
     </Form>
-  );
-};
+    )
+  }
+
+}
+
+// const TileForm = () => {
+//   const [launchDate, setLaunchDate] = useState("");
+//   const [status, setStatus] = useState("");
+
+//   const onSubmit = async (e) => {
+//     e.preventDefault();
+//     console.log(e);
+//     // await TilesDataService.createTile(launchDate, status);
+//     // post method to API with form data
+//   };
+
+//   return (
+//     <Form onSubmit={onSubmit}>
+//       <Form.Group>
+//         <Form.Label>Launch Date</Form.Label>
+//         <Form.Control
+//           id="date-input"
+//           type="date"
+//           onChange={setLaunchDate}
+//         />
+//       </Form.Group>
+
+//       <Form.Group controlId="formBasicCheckbox">
+//         <Form.Check type="checkbox" label="Check me out" />
+//       </Form.Group>
+
+//       <Form.Group>
+//         <Form.Label>Status</Form.Label>
+//         <Form.Control
+//           id="status-select"
+//           as="select"
+//           onChange={setStatus}
+//         >
+//           <option>-- Choose an option --</option>
+//           <option value="PENDING">Pending</option>
+//           <option value="ARCHIVE">Archive</option>
+//           <option value="LIVE">Live</option>
+//         </Form.Control>
+//       </Form.Group>
+//       <Button variant="primary" type="submit">
+//         Submit
+//       </Button>
+//     </Form>
+//   );
+// };
 
 export default TileForm;
