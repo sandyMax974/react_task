519f7992f86e17acc7d90f39315f7b9f86e88902
sandy <pounoussamy.sandy@gmail.com>
2021-06-26 11:15:05 +0100

    Added defaultValue to be prefilled in form when updating tile info

--- src/Components/TileUpdateForm.js
+++ src/Components/TileUpdateForm.js
@@ -1,43 +1,55 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from "react";
 import { Button, Form } from "react-bootstrap";
 import TilesDataService from "../Services/TilesData.service";
 
-const TileUpdateForm = ({ date, status}) => {
-  const [updatedDate, setUpdatedDate] = useState(date)
-  const [updatedStatus, setUpdatedStatus] = useState(status)
-  console.log(date)
+const TileUpdateForm = ({ tileId }) => {
+  const [date, setDate] = useState("");
+  const [status, setStatus] = useState("");
+  const [updatedDate, setUpdatedDate] = useState(date);
+  const [updatedStatus, setUpdatedStatus] = useState(status);
 
-{/*  const getTileData = async (id) => {
-    const tileData = await TilesDataService.getTile(id);
-    setDate(tileData.data.launchDate);
-    setStatus(tileData.data.status);
-  }
-getTileData(tileId); */}
+  useEffect(() => {
+    const getTileData = async (id) => {
+      const tileData = await TilesDataService.getTile(id);
+      setDate(tileData.data.launchDate);
+      setStatus(tileData.data.status);
+    };
+    getTileData(tileId);
+  }, []);
 
-  const handleSubmit = () => {
-  }
+  const handleSubmit = () => {};
 
   return (
-  <Form onSubmit={handleSubmit}>
-    <Form.Group>
-      <Form.Label>Launch Date</Form.Label>
-      <Form.Control name="date"type="date" onChange={(e) => setUpdatedDate(e.target.value)} />
-    </Form.Group>
+    <Form onSubmit={handleSubmit}>
+      <Form.Group>
+        <Form.Label>Launch Date</Form.Label>
+        <Form.Control
+          name="date"
+          type="date"
+          defaultValue={date}
+          onChange={(e) => setUpdatedDate(e.target.value)}
+        />
+      </Form.Group>
 
-    <Form.Group>
-      <Form.Label>Status</Form.Label>
-      <Form.Control name="status" as="select" onChange={(e) => setUpdatedStatus(e.target.value)} >
-        <option>-- Choose an option --</option>
-        <option value="pending">Pending</option>
-        <option value="archive">Archive</option>
-        <option value="live">Live</option>
-      </Form.Control>
-    </Form.Group>
-    <Button variant="primary" type="submit">
+      <Form.Group>
+        <Form.Label>Status</Form.Label>
+        <Form.Control
+          name="status"
+          as="select"
+          defaultValue={status}
+          onChange={(e) => setUpdatedStatus(e.target.value)}
+        >
+          <option>-- Choose an option --</option>
+          <option value="pending">Pending</option>
+          <option value="archive">Archive</option>
+          <option value="live">Live</option>
+        </Form.Control>
+      </Form.Group>
+      <Button variant="primary" type="submit">
         Submit
-    </Button>
-  </Form>
-  )
-}
+      </Button>
+    </Form>
+  );
+};
 
-export default TileUpdateForm
+export default TileUpdateForm;
--- src/Components/UpdateTileModal.js
+++ src/Components/UpdateTileModal.js
@@ -1,51 +1,48 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { Modal, Button } from "react-bootstrap";
-import TileUpdateForm from "./TileUpdateForm"
+import TileUpdateForm from "./TileUpdateForm";
 import TilesDataService from "../Services/TilesData.service";
 
-const UpdateTileModal = ({tileId}) => {
+const UpdateTileModal = ({ tileId }) => {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
-  // const handleShow = () => setShow(true);
+  const handleShow = () => setShow(true);
 
-  const [date, setDate] = useState("")
-  const [status, setStatus] = useState("")
+  //   const [date, setDate] = useState("")
+  //   const [status, setStatus] = useState("")
 
-const handleShow = () => {
-  const getTileData = async (id) => {
-    const tileData = await TilesDataService.getTile(id);
-    setDate(tileData.data.launchDate);
-    setStatus(tileData.data.status);
-  }
-  setShow(true);
-  getTileData(tileId);
-}
+  // const handleShow = () => {
+  //   const getTileData = async (id) => {
+  //     const tileData = await TilesDataService.getTile(id);
+  //     setDate(tileData.data.launchDate);
+  //     setStatus(tileData.data.status);
+  //   }
+  //   setShow(true);
+  //   getTileData(tileId);
+  // }
 
   return (
     <>
-    <Button variant="secondary" onClick={handleShow}>
-      Update
-    </Button>
+      <Button variant="secondary" onClick={handleShow}>
+        Update
+      </Button>
 
-    <Modal show={show} onHide={handleClose}>
-      <Modal.Header closeButton>
-        <Modal.Title>Update tile details</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <TileUpdateForm tileId={tileId} date={date} status={status}/>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={handleClose}>
-          Close
-        </Button>
-        <Button variant="primary" onClick={handleClose}>
-          Save Changes
-        </Button>
-      </Modal.Footer>
-    </Modal>
-  </>
-  )
-}
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Update tile details</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <TileUpdateForm tileId={tileId} /> {/* date={date} status={status} */}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
 
-export default UpdateTileModal
+export default UpdateTileModal;
